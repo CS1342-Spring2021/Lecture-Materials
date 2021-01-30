7f8945aa0a13100f9dbd39b6597e38efc7f4149e
Erik Gabrielsen <egabrielsen@smu.edu>
2021-01-30 10:10:26 -0600

    uploading chapter 1 and 2

---
+++ Chapter02/modulo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Integer division and modulo 
+  int x = 5;
+  int y = 5;
+
+  int result = x % y;
+
+  // if (x % 2 == 0)
+
+  cout << "Result: " << result << endl;
+  
+  return 0;
+}
